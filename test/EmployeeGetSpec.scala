06a82f2410af70d64faef7c521ab3f091286d671
k-miyake <3katsu3hiro@gmail.com>
2014-09-15 16:11:57 +0900

    不要なサンプルファイルを削除。テストを落ちるように修正。

--- test/EmployeeGetSpec.scala
+++ test/EmployeeGetSpec.scala
@@ -6,12 +6,12 @@ import play.api.test._
 
 class EmployeeGetSpec extends Specification {
 
-  "EmployeeへのGET" should {
-    "IDのEmployeeがある場合" in new WithApplication{
-      val response = route(FakeRequest(GET, "/employee/1")).get
+  "EmployeeをIDで取得した場合" should {
+    "そのIDのEmployeeを取得する" in new WithApplication{
+      val response = route(FakeRequest(GET, "/employee/2")).get
       status(response) must equalTo(OK)
       contentType(response) must beSome.which(_ == "application/json")
-      contentAsString(response) must contain ("""{"employee":{"id":1,"name":"bob"}}""")
+      contentAsString(response) must contain ("""{"employee":{"id":2,"name":"tom"}}""")
     }
   }
 }
