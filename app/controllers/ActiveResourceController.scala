fba3bfd65f04c2221a327aedd782545ad07344c3
k-miyake <3katsu3hiro@gmail.com>
2014-09-18 22:45:16 +0900

    EmployeeのGETをリファクタリング。コントローラからモデルにJSON化の処理を移動

--- app/controllers/ActiveResourceController.scala
+++ app/controllers/ActiveResourceController.scala
@@ -7,7 +7,7 @@ import play.api.libs.json._
 import play.api.libs.functional.syntax._
 
 
-object EmployeeController extends Controller {
+object ActiveResourceController extends Controller {
 
   implicit val employeeWrites = new Writes[Employee] {
     def writes(employee: Employee) = Json.obj("id" -> employee.id, "name" -> employee.name)
