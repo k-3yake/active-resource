a77499b26205a898c94803a872afe7fbc430e310
k-miyake <3katsu3hiro@gmail.com>
2014-09-19 08:02:14 +0900

    EmployeeのGETの細かいリファクタ

--- app/model/Employee.scala
+++ app/model/Employee.scala
@@ -11,26 +11,24 @@ import play.api.libs.json.{JsValue, Json, Writes}
  */
 
 class Employee(val id: Int,val name: String){
-  val con = DB.getConnection()
-  def toJsValue() = {
-    implicit val employeeWrites = new Writes[Employee] {
-      def writes(employee: Employee) = Json.obj("id" -> employee.id, "name" -> employee.name)
-    }
-    Json.toJson(Map("Employee" -> this))
+  implicit val writes = new Writes[Employee] {
+    def writes(employee: Employee) = Json.obj("id" -> employee.id, "name" -> employee.name)
   }
-  def unapply(employee: Employee) = {
-    (employee.id,employee.name)
+  def toJsValue() = {
+    Json.toJson(Map(Employee.ResourceName -> this))
   }
 }
 
 object Employee {
+  def ResourceName = "Employee"
+  val con = DB.getConnection()
+  val parser = for {
+    id <- int("id")
+    name <- str("name")
+  } yield new Employee(id,name)
   def find(id: Int) = {
     DB.withConnection { implicit c =>
-      val parser = for {
-        id <- int("id")
-        name <- str("name")
-      } yield (new Employee(id,name))
-      SQL("select * from Employee where id = {id}").on('id -> id).as(parser.single)
+      SQL("select * from " + ResourceName + " where id = {id}").on('id -> id).as(parser.single)
     }
   }
 }
--- test/EmployeeGetSpec.scala
+++ test/EmployeeGetSpec.scala
@@ -13,15 +13,15 @@ class EmployeeGetSpec extends Specification {
   trait employeeSetupApplication extends WithApplication with Scope {
     DB.withConnection { implicit c =>
       SQL("delete from Employee").executeUpdate()
-      val insertSql: SqlQuery = SQL("insert into Employee(ID,NAME) values ({id}, {name})")
-      insertSql.on('id -> "1", 'name -> "bob").executeInsert()
-      insertSql.on('id -> "2", 'name -> "tom").executeInsert()
+      val insert = SQL("insert into Employee(ID,NAME) values ({id}, {name})")
+      insert.on('id -> "1", 'name -> "bob").executeInsert()
+      insert.on('id -> "2", 'name -> "tom").executeInsert()
     }
   }
 
   "IDでのEmployeeの取得" should {
     "そのIDのEmployeeを返す" in new employeeSetupApplication{
-      val response = route(FakeRequest(GET, "/employee/2")).get
+      val response = route(FakeRequest(GET, "/Employee/2")).get
       status(response) must equalTo(OK)
       contentType(response) must beSome.which(_ == "application/json")
       contentAsString(response) must contain ("""{"Employee":{"id":2,"name":"tom"}}""")
